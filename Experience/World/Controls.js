552c8d7ef128a148618bc24dd4a2cd8a6bf00fa4
tankamout <daguguguu@gmail.com>
2022-09-17 14:11:08 +0630

    control update

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,8 +1,7 @@
 import Experience from "../experience";
 import * as THREE from "three";
-import GSAP from 'gsap'
+import GSAP from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 
 export default class Controls {
   constructor() {
@@ -11,42 +10,20 @@ export default class Controls {
     this.resources = this.experience.resources;
     this.time = this.experience.times;
     this.camera = this.experience.camera;
-    this.sizes=this.experience.sizes
+    this.sizes = this.experience.sizes;
 
-    GSAP.registerPlugin(ScrollTrigger)
-    this.room=this.experience.world.room.actualRoom
-    this.setPath()
-
-
- 
-
+    GSAP.registerPlugin(ScrollTrigger);
+    this.room = this.experience.world.room.actualRoom;
+    this.setScrollTrigger();
   }
 
- setPath(){
-  this.timeline=new GSAP.timeline()
-  this.timeline.to(this.room.position,{
-    x:()=>{
-      return this.sizes.width * 0.0012 
-    },
-    scrollTrigger:{
-      trigger:'.first-move',
-      markers:true,
-      start:"top top",
-      end:"bottom bottom",
-      scrub:1,
-      invalidateOnRefresh:true,
-    },
-
-  })
- }
-
-
-
-
+  setScrollTrigger() {
+    ScrollTrigger.matchMedia({
+      "(min-width:960px)": function () {},
+      "(max-width:960px)": function () {},
+    });
+  }
 
   resize() {}
-  update() {
- 
-
-  }
+  update() {}
 }
